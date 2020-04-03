d6c334b2a270d4fd917fcef6217e36235ed23c2f
jeinsei <juliennoe.job@gmail.com>
2020-04-03 21:41:39 +0200

    First Commit

---
+++ UNITY_LEARN_VR/Assets/_SCRIPTS/ReticleParameter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReticleParameter : MonoBehaviour
+{
+    public Camera raycastCamera; // Camera de reference pour le raycast
+    public Image mainTarget; // icone du pointer
+    public Sprite hitOff; // changement d'état hit on
+    public Sprite hitOn; // changement d'état hit off
+    public Sprite hitLoad; // changement d'état durant la teleportation
+    public Sprite hitButton;
+    public float waitingTime; // temps d'attente du timer durant le chargement de l'image
+    public float gvrTimer; // Timer de l'icone
+    public bool gvrStatuts; // Boolean du statut du chargement
+                  
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycastCamera.transform.position, raycastCamera.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(raycastCamera.transform.position, raycastCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            mainTarget.GetComponent<Image>().sprite = hitOn;
+            mainTarget.GetComponent<Animator>().Play("hitOn");
+
+        }
+        else
+        {
+            Debug.DrawRay(raycastCamera.transform.position, raycastCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+            mainTarget.GetComponent<Image>().sprite = hitOff;
+            mainTarget.GetComponent<Animator>().Play("hitOff");
+            mainTarget.fillAmount = 1;
+        }
+
+    }
+}
---
+++ UNITY_LEARN_VR/Assets/_SCRIPTS/TeleportItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportItem : MonoBehaviour
+{
+
+    public void TeleportPlayer()
+    {
+        var player = GameObject.Find("ManagerScript").GetComponent<Teleportation>().player;
+
+        player.transform.position = new Vector3(transform.position.x, transform.position.y +1.5f, transform.position.z);
+
+        Debug.Log("TeleportPlayer");
+    }
+}
